1c3c1b38eda6ca36befd43c85e7a38dadfc4df2a
Petter1998 <petterarevalo1998@gmail.com>
2024-04-01 17:37:02 -0500

    Conectando aplicativo a Firebase

---
+++ Inicio_Sesion/registro_inicio.js
@@ -0,0 +1,95 @@
+//Escucha eventos de teclado (keyup y change), enfocar (focus) y desenfocar (blur) en los campos de entrada 
+//de un formulario.
+$('.form').find('input').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup'|| e.type === 'change') { // Añadir chequeo para 'change'
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+
+
+//Al cargar la página, añade la clase active a la etiqueta que precede inmediatamente a cualquier 
+//campo de entrada de tipo fecha, y asegura que esta etiqueta permanezca activa independientemente 
+//de si el campo de fecha está enfocado o desenfocado.
+$(document).ready(function() {
+  // Selecciona el label que precede inmediatamente a mi input de tipo 'date'
+  var dateLabel = $('input[type="date"]').prev('label');
+
+  // Añade  la clase 'active' al cargar la página
+  dateLabel.addClass('active');
+
+  // Manejar eventos de focus y blur
+  $('input[type="date"]').on('focus blur', function(e) {
+    // Mantener la etiqueta siempre activa
+    dateLabel.addClass('active');
+  });
+});
+
+
+
+//Maneja los clics en las pestañas dentro de una interfaz de usuario de pestañas.
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+
+
+// Referencia al formulario
+const form = document.querySelector('#registroForm');
+
+form.addEventListener('submit', (e) => {
+  // Prevén el comportamiento predeterminado de envío del formulario
+  e.preventDefault();
+
+  // Agrega un nuevo documento con una ID generada
+  addDoc(collection(db, "Usuarios"), {
+    Nombres: form['Nombres'].value,
+    Apellidos: form['Apellidos'].value,
+    Identificación: form['Identificación'].value,
+    Teléfono: form['Telefono'].value,
+    Usuario: form['Usuario'].value,
+    Cargo: form['Cargo'].value,
+    Correo: form['Email'].value,
+    Contraseña: form['Password'].value
+  }).then(() => {
+    // Limpia el formulario o redirige al usuario
+    form.reset();
+    console.log("Documento añadido con éxito");
+  }).catch((error) => {
+    console.error("Error al añadir el documento: ", error);
+  });
+});
